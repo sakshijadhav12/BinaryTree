76b0dd2d9226feba819ffae69aeb2125893315ab
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-31 13:24:33 +0530

    Ability to create a BST by adding 56 and then adding 30 & 70

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace BinaryTree
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTableAndTree
 {
-    internal class Program
+    internal class BinaryTree
     {
-        static void Main(string[] args)
+        public static void Main(String[] args)
         {
-            Console.WriteLine("Binary Tree");
+            Console.WriteLine("Welcome to Binary Tree Program...");
+
+            BinarySearchTree bst = new BinarySearchTree();
+
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+
+            Console.WriteLine("Inorder Traversal:");
+            bst.InorderTraversal();
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        private Node root;
+
+        public void Insert(int value)
+        {
+            root = InsertNode(root, value);
+        }
+
+        private Node InsertNode(Node currentNode, int value)
+        {
+            if (currentNode == null)
+            {
+                return new Node(value);
+            }
+
+            if (value < currentNode.Value)
+            {
+                currentNode.Left = InsertNode(currentNode.Left, value);
+            }
+            else if (value > currentNode.Value)
+            {
+                currentNode.Right = InsertNode(currentNode.Right, value);
+            }
+
+            return currentNode;
+        }
+
+        public void InorderTraversal()
+        {
+            InorderTraversal(root);
+        }
+
+        private void InorderTraversal(Node currentNode)
+        {
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            InorderTraversal(currentNode.Left);
+            Console.WriteLine(currentNode.Value);
+            InorderTraversal(currentNode.Right);
+        }
+
+        private class Node
+        {
+            public int Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
         }
     }
 }
