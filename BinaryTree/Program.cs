076c569a7fb889d358bd75f060b4a05797375d83
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-31 13:28:00 +0530

    Ability to create the binary tree

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
 namespace HashTableAndTree
 {
     internal class BinaryTree
@@ -8,31 +7,42 @@ namespace HashTableAndTree
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Binary Tree Program...");
-
             BinarySearchTree bst = new BinarySearchTree();
-
             bst.Insert(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(3);
+            bst.Insert(16);
+            bst.Insert(65);
+            bst.Insert(63);
+            bst.Insert(67);
 
             Console.WriteLine("Inorder Traversal:");
             bst.InorderTraversal();
+
+            Console.WriteLine("Total number of nodes: " + bst.Size());
         }
     }
 
     public class BinarySearchTree
     {
         private Node root;
+        private int count;
 
         public void Insert(int value)
         {
             root = InsertNode(root, value);
         }
-
         private Node InsertNode(Node currentNode, int value)
         {
             if (currentNode == null)
             {
+                count++;
                 return new Node(value);
             }
 
@@ -44,25 +54,26 @@ namespace HashTableAndTree
             {
                 currentNode.Right = InsertNode(currentNode.Right, value);
             }
-
             return currentNode;
         }
-
         public void InorderTraversal()
         {
             InorderTraversal(root);
         }
-
         private void InorderTraversal(Node currentNode)
         {
             if (currentNode == null)
             {
                 return;
             }
-
             InorderTraversal(currentNode.Left);
             Console.WriteLine(currentNode.Value);
             InorderTraversal(currentNode.Right);
+        }
+
+        public int Size()
+        {
+            return count;
         }
 
         private class Node
@@ -70,7 +81,6 @@ namespace HashTableAndTree
             public int Value { get; }
             public Node Left { get; set; }
             public Node Right { get; set; }
-
             public Node(int value)
             {
                 Value = value;
